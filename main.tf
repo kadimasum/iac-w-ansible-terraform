384559f5153363cc5e1b99fe632e133534e28057
kadimasum <kadimasam@gmail.com>
2026-04-01 15:18:21 +0300

    [refactor and import modules]

--- main.tf
+++ main.tf
@@ -1,71 +1,53 @@
-provider "aws" {
-  region = "us-east-1"
+terraform {
+  required_version = ">= 1.1.0"
 
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 5.0"
+    }
+  }
 }
 
+provider "aws" {
+  region = var.aws_region
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
+}
 
-resource "aws_security_group" "server_sg" {
+module "server_sg" {
+  source      = "./modules/security_group"
   name        = "server_sg"
-  description = "Security group for the EC2 instances"
+  description = "Security group for EC2 instances"
 
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+  ingress = [
+    { from_port = 22, to_port = 22, protocol = "tcp", cidr_blocks = ["0.0.0.0/0"] },
+    { from_port = 80, to_port = 80, protocol = "tcp", cidr_blocks = ["0.0.0.0/0"] },
+    { from_port = 443, to_port = 443, protocol = "tcp", cidr_blocks = ["0.0.0.0/0"] },
+    { from_port = -1, to_port = -1, protocol = "icmp", cidr_blocks = ["0.0.0.0/0"] },
+  ]
 
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = -1
-    to_port     = -1
-    protocol    = "icmp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+  egress = [
+    { from_port = 0, to_port = 0, protocol = "-1", cidr_blocks = ["0.0.0.0/0"] },
+  ]
 
   tags = {
     Name = "server_sg"
   }
 }
 
-
-resource "aws_instance" "server1" {
-  ami           = "ami-0cd59ecaf368e5ccf"
-  security_groups = [aws_security_group.server_sg.name]
-  instance_type = "t2.micro"
-
-  tags = {
-    Name = "server1"
-  }
+locals {
+  server_names = ["server1", "server2"]
 }
 
-
-resource "aws_instance" "server2" {
-  ami             = "ami-0cd59ecaf368e5ccf"
-  instance_type   = "t2.micro"
-  security_groups = [aws_security_group.server_sg.name]
-
+module "servers" {
+  source                 = "./modules/instance"
+  for_each               = toset(local.server_names)
+  name                   = each.key
+  ami                    = "ami-0cd59ecaf368e5ccf"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [module.server_sg.id]
   tags = {
-    Name = "server2"
+    environment = "dev"
   }
 }
