1dbb23ec2d451fc824c42c91adf07cb1f0ea0b72
kadimasum <kadimasam@gmail.com>
2026-04-01 15:17:31 +0300

    [Add variables]

---
+++ variables.tf
@@ -0,0 +1,15 @@
+variable "aws_region" {
+  type        = string
+  description = "AWS region to deploy resources"
+  default     = "us-east-1"
+}
+
+variable "aws_access_key" {
+  type        = string
+  description = "AWS access key"
+}
+
+variable "aws_secret_key" {
+  type        = string
+  description = "AWS secret key"
+}
