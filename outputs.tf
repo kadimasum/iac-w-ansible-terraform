8ea0a3197ffb0637d8bf0efe7a5351dd0e116b42
kadimasum <kadimasam@gmail.com>
2026-04-01 16:06:28 +0300

    [output server ip vars]

--- outputs.tf
+++ outputs.tf
@@ -5,3 +5,8 @@ output "security_group_id" {
 output "server_ids" {
   value = { for k, m in module.servers : k => m.id }
 }
+
+output "server_public_ips" {
+  value = { for k, m in module.servers : k => m.public_ip }
+}
+
